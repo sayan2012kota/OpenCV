95d04f5cd6e1ce9b42377f3556e8c0736052be60
sayan2012kota <sayankota2012@gmail.com>
2026-08-16 09:58:05 +0100

    Add files via upload

---
+++ invisible.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+video = cv2.VideoCapture(r"C:\Users\pooja\Desktop\OpenCV\images\Invisible man video.mp4")
+background = None
+
+for i in range(10):
+    boolean, frame = video.read()
+    if boolean == True:
+        background = frame
+video.release()
+flipped_background = np.flip(background, axis = 1)
+
+while video.isOpened():
+    boolean2, frames = video.read()
+    flipped_frame = np.flip(frames, axis = 1)
+    image =  cv2.cvtColor(flipped_frame, cv2.COLOR_BGR2HSV)
+    red_values = np.array([0, 120, 40])
+    highest_red_value = np.array([100, 255, 255])
+    mask = cv2.inRange(image, red_values, highest_red_value)
+    lower_red_range = np.array([170, 40, 40])
+    highest_red_range = np.array([180, 255, 255])
+    mask2 = cv2.inRange(image, lower_red_range, highest_red_range)
+    mask = mask + mask2
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3,3), np.uint8), iterations=1)
+    mask2 = cv2.bitwise_not(mask)
+
